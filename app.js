803dfd20625af0398f150aa42b104416a15023e2
Esther White <esthersoftwaredev@gmail.com>
2025-04-21 20:00:07 +0300

    changed to swapi.tech due to issues with swapi.dev

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const btn2 = document.getElementById("btn2");
 function getSwapi() {
   const randomNumber = Math.ceil(Math.random() * 83);
 
-  fetch(`https://swapi.dev/api/people/${randomNumber}`)
+  fetch(`https://swapi.tech/api/people/${randomNumber}`)
     .then((response) => response.json())
     .then((character) => {
-      console.log(character);
+      console.log(character.result.properties);
+      const charProperties = character.result.properties;
       document.getElementById(
         "output1"
-      ).innerHTML = `<h3><i>Name: </i>${character.name}</h3>
-      <ul><li><i>Birth Year: </i>${character.birth_year}</li>
-      <li><i>Hair Color: </i>${character.hair_color}</li>
-      <li><i>Eye Color: </i>${character.eye_color}</li>
-      <li><i>Gender: </i>${character.gender}</li>
+      ).innerHTML = `<h3><i>Name: </i>${charProperties.name}</h3>
+      <ul><li><i>Birth Year: </i>${charProperties.birth_year}</li>
+      <li><i>Hair Color: </i>${charProperties.hair_color}</li>
+      <li><i>Eye Color: </i>${charProperties.eye_color}</li>
+      <li><i>Gender: </i>${charProperties.gender}</li>
       </ul>`;
     });
 }
